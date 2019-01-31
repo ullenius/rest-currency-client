76b33e78820a196623daa3c53666e142eb693728
Anosh D. Ullenius <anosh@anosh.se>
2019-01-31 19:08:44 +0100

    First commit. First functional version

---
+++ src/se/anosh/restcurrencyclient/RestClient.java
@@ -0,0 +1,103 @@
+ /**
+  *
+  * ITHS 2019 Rest client exercise
+  *
+  * Obtains currency exchange rates in JSON from server using
+  * REST and stores them as a POJO (plain old java object)
+  * 
+  * Uses: https://exchangeratesapi.io/
+  * 
+  * Thanks to Madis Väin (EST) for the API :)
+  *
+  */
+
+package se.anosh.restcurrencyclient;
+
+import se.anosh.restcurrencyclient.domain.ExchangeRate;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Anosh D. Ullenius <anosh@anosh.se>
+ */
+public class RestClient {
+    
+    private String url = "https://api.exchangeratesapi.io/latest";
+    
+    public static void main(String[] args) {
+        
+        RestClient client = new RestClient();
+        client.run();
+    }
+    
+    private void run() {
+        
+        System.out.println("RestCurrency - Foreign Exchange rates REST-client\n");
+       
+        menu(); // user IO
+        
+        Client client = ClientBuilder.newClient();
+        
+        WebTarget target = client.target(url);
+        Invocation invocation = target.request("application/JSON").buildGet();
+        Response response = invocation.invoke();
+        
+        System.out.println(response.getHeaders().toString());
+        System.out.println(response.getStatus());
+        
+        String result = response.readEntity(String.class);
+
+        //System.out.println(result);
+        
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        ExchangeRate valuta = gson.fromJson(result, ExchangeRate.class);
+        response.close();
+        
+        System.out.println("Base: " + valuta.getBase());
+        System.out.println("Date: " + valuta.getDate());
+        
+        Map<String,Double> map = new HashMap(valuta.getRates());
+        for (String key : map.keySet()) {
+            System.out.println(key + "\t" + map.get(key));
+        }
+        
+    }
+    
+    private void menu() {
+        
+        Scanner sc = new Scanner(System.in);
+        int input = 1;
+        final List<String> codes = CurrencyCodes.getAllCodes();
+        final int size = codes.size();
+        
+        System.out.println("Menu");
+        System.out.println("1. Use € Euro as base currency");
+        System.out.println("2. Choose different base currency");
+        
+        // Prints the list
+        for (int i = 0; i < codes.size(); i++) {
+            System.out.println((i+1) + "\t" + codes.get(i));
+        }
+        
+        do {
+            System.out.println("\nPlease make your selection: ");
+            input = sc.nextInt();
+        } while (input > size || input < 1);
+        
+        if (input != 1)
+            url = url.concat("?base=" + codes.get(input-1));
+        sc.close(); // closes Scanner
+    }
+    
+    
+}
---
+++ src/se/anosh/restcurrencyclient/domain/ExchangeRate.java
@@ -0,0 +1,54 @@
+package se.anosh.restcurrencyclient.domain;
+
+import java.util.Collections;
+import java.util.Map;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * 
+ * This is a domain class used for JSON serialization & de-serialization
+ * 
+ *
+ * @author Anosh D. Ullenius <anosh@anosh.se>
+ */
+@XmlRootElement
+public class ExchangeRate {
+    
+    private String base;
+    private String date;
+    private Map<String,Double> rates;
+    
+    public ExchangeRate() {
+        
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Map<String, Double> getRates() {
+        return Collections.unmodifiableMap(rates);
+    }
+
+    public void setRates(Map<String, Double> rates) {
+        this.rates = rates;
+    }
+    @Override
+    public String toString() {
+        return "Currency{" + "base=" + base + ", date=" + date + ", rates=" + rates + '}';
+    }
+
+    
+}
